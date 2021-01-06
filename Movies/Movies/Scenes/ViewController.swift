a967c8210bd6fa703d833e657f712a90993b3770
daniel.avalos <jdanielavalosa@gmail.com>
2021-01-06 11:43:29 -0500

    Project base added

---
+++ Movies/Movies/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by DESARROLLO on 6/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

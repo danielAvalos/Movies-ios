16d8e826a2174f4351faf721ffef6db1f663fd58
daniel.avalos <jdanielavalosa@gmail.com>
2021-01-07 20:54:00 -0500

    UI MovieDetail updated

--- Movies/Movies/Scenes/MovieDetail/MovieDetailViewController.swift
+++ Movies/Movies/Scenes/MovieDetail/MovieDetailViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 final class MovieDetailViewController: UIViewController {
 
+    // MARK: - IB Outlets
+
+    @IBOutlet private weak var image: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var popularityLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
